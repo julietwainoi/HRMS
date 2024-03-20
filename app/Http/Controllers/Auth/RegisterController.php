083607432105651a0a5f7ff8ba2013dcd6a6d4ef
julietwainoi <juliewainoi@gmail.com>
2024-03-20 12:28:32 +0300

    HRMS SYSTEM

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use App\Rules\CheckClientExists;
+
 
 class RegisterController extends Controller
 {
@@ -52,7 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'IDNo' => ['required', new CheckClientExists], // Add custom rule for ID
+            'IDNo' => ['required', ], // Add custom rule for ID
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             
         ]);
